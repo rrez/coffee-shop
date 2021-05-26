1d3be8b424d96212aec8c1727ddbec7b279d39c2
Robert Rez <thebigtony@gmail.com>
2021-05-26 18:51:06 +0200

    test

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,7 @@ export default function Layout({ children }) {
     <div>
       <header id={styles.header}>
         <div id={styles.inner}>
-          <h1>Joe's Coffee Shop</h1>
+          <h1>Roberts Coffee Shop</h1>
         </div>
       </header>
       <main id={styles.main}>
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,6 +5,7 @@ import { graphql, useStaticQuery } from 'gatsby';
 import Layout from '../components/Layout';
 
 import styles from './index.module.css';
+import BlogList from '../components/BlogList';
 
 export default function IndexPage() {
   const data = useStaticQuery(graphql`
@@ -22,6 +23,7 @@ export default function IndexPage() {
       <div id={styles.hero}>
         <h1>{data.site.siteMetadata.title}</h1>
       </div>
+      <BlogList></BlogList>
     </Layout>
   );
 }
